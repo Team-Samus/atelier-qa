2fb4b0f5e38158b0ac9c47db76875ab4d3ea9c0e
Walter Latimer <walter@walterlatimer.com>
2022-04-23 17:50:27 -0400

    Initial commit

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS questions, answers, photos;
+
+CREATE TABLE questions (
+  question_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  product_id INTEGER NOT NULL,
+  question_body TEXT NULL DEFAULT NULL,
+  question_date BIGINT,
+  asker_name VARCHAR(50) NOT NULL,
+  asker_email VARCHAR(60) NOT NULL,
+  reported BOOLEAN NOT NULL DEFAULT FALSE,
+  question_helpfulness INTEGER NOT NULL
+);
+
+CREATE TABLE answers (
+  answer_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  question_id INTEGER NOT NULL REFERENCES questions(question_id),
+  body TEXT NOT NULL,
+  date BIGINT,
+  answerer_name VARCHAR(50) NOT NULL,
+  answerer_email VARCHAR(60) NOT NULL,
+  reported BOOLEAN NOT NULL DEFAULT FALSE,
+  helpfulness INTEGER NOT NULL
+);
+
+CREATE TABLE photos (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  answer_id INTEGER NOT NULL REFERENCES answers(answer_id),
+  url VARCHAR(300) NOT NULL
+);
+
+\COPY questions FROM 'RELATIVE PATH' WITH DELIMITER ',' NULL 'null' CSV HEADER;
+\COPY answers FROM 'RELATIVE PATH' WITH DELIMITER ',' NULL 'null' CSV HEADER;
+\COPY photos FROM 'RELATIVE PATH' WITH DELIMITER ',' NULL 'null' CSV HEADER;
